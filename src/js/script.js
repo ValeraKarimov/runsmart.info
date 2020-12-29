7c4068c72aa8d9ef31d9e17a87a130a2d0fe5922
Valerii Karimov <valeraguess@gmail.com>
2020-12-29 18:05:04 +0200

    add pageup element and smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -99,4 +99,36 @@ const slider = tns({
 
   $('input[name=phone]').mask('+7 (999) 999-99-99');
 
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialise()
+    }).done(function() {
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function() {
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
 });
